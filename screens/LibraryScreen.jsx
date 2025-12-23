8a212e59ffa6c3a48e3170f219e80ad25be54523
hachtek98-stack <hachtek98@gmail.com>
2025-12-23 18:41:38 +0300

    Ajout de l'écran LibraryScreen

---
+++ screens/LibraryScreen.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const LibraryScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Écran de bibliothèque</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+});
+
+export default LibraryScreen;
