e1f0466e020119d0776f593fc0ff69cad5b826ee
hachtek98-stack <hachtek98@gmail.com>
2025-12-23 18:41:33 +0300

    Ajout de l'écran DiscoverScreen

---
+++ screens/DiscoverScreen.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const DiscoverScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Écran de découverte</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 24,
+  },
+});
+
+export default DiscoverScreen;
